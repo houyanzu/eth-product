28dc6edfdce4e008c36feafd1e8a61037c55832d
contract <931953294@qq.com>
2022-06-09 10:40:45 +0800

    .

--- app/transfer/transfer.go
+++ app/transfer/transfer.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/houyanzu/eth-product/config"
@@ -13,6 +14,7 @@ import (
 	"github.com/houyanzu/eth-product/database/transferdetails"
 	"github.com/houyanzu/eth-product/database/transferrecords"
 	"github.com/houyanzu/eth-product/lib/contract/multitransfer"
+	"github.com/houyanzu/eth-product/lib/contract/standardcoin"
 	"github.com/houyanzu/eth-product/lib/crypto/aes"
 	"github.com/houyanzu/eth-product/tool/eth"
 	"math/big"
@@ -129,5 +131,68 @@ func Transfer(limit int) (err error) {
 	tr.Add()
 
 	transferdetails.New(nil).SetExec(ids, tr.Data.ID)
+	return
+}
+
+func SingleTransfer(token string, to string, amount *big.Int) (hash string, err error) {
+	conf := config.GetConfig()
+	client, err := ethclient.Dial(conf.Eth.Host)
+	if err != nil {
+		return
+	}
+
+	privateKey, err := crypto.HexToECDSA(privateKeyStr)
+	if err != nil {
+		return
+	}
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		return
+	}
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.NonceAt(context.Background(), fromAddress, nil)
+	if err != nil {
+		return
+	}
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		return
+	}
+
+	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, big.NewInt(conf.Eth.ChainId))
+	if err != nil {
+		return
+	}
+	if token == "0x0000000000000000000000000000000000000000" {
+		tx := types.NewTransaction(nonce, common.HexToAddress(to), amount, 21000, gasPrice, nil)
+		var signedTx *types.Transaction
+		signedTx, err = types.SignTx(tx, types.NewEIP155Signer(big.NewInt(conf.Eth.ChainId)), privateKey)
+		if err != nil {
+			return
+		}
+
+		err = client.SendTransaction(context.Background(), signedTx)
+		if err != nil {
+			return
+		}
+		hash = signedTx.Hash().Hex()
+		return
+	}
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)     // in wei
+	auth.GasLimit = uint64(150000) // in units
+	auth.GasPrice = gasPrice
+
+	tokenCon := common.HexToAddress(token)
+	tokenInstance, err := standardcoin.NewStandardcoin(tokenCon, client)
+	if err != nil {
+		return
+	}
+	tx, err := tokenInstance.Transfer(auth, common.HexToAddress(to), amount)
+	if err != nil {
+		return
+	}
+	hash = tx.Hash().Hex()
 	return
 }
